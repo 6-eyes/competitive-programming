b7adbbf71f40772cb5aa1be5c2026bbab3a5f3aa
6-eyes <prateek.n2000@ymail.com>
2026-07-12 14:41:19 +0530

    abc466_c

--- atcoder/abc466/c.rs
+++ atcoder/abc466/c.rs
@@ -4,21 +4,46 @@ fn main() -> Result<(), Error> {
     let mut s = String::new();
     stdin().read_line(&mut s).map_err(Error::Input)?;
 
-    let n = s.trim().parse::<u16>().map_err(Error::Parse)?;
+    let n = s.trim().parse::<u32>().map_err(Error::Parse)?;
+    // 1...2...3...4...5...6...7...8
+    //  0.1 0.9 0.5 1.8 0.2 0.1 1.1
+    // 1 -> 1 2 3
+    // 1 2 3 4
+    let mut l = 1;
+    let mut r = 2;
     let mut ans = 0;
-    for (x, y) in (1..=n).flat_map(|x| (x + 1..=n).map(move |y| (x, y))) {
-        println!("? {} {}", x, y);
+
+    while r <= n {
+        println!("? {} {}", l, r);
 
         // read the input
         let mut s = String::new();
         stdin().read_line(&mut s).map_err(Error::Input)?;
 
         match s.as_str().trim() {
-            "Yes" => ans += 1,
-            "No" => {},
+            // distance between l and r is atmost 1
+            // continue exploring
+            "Yes" => {
+                r += 1;
+            },
+            // distance between l and r is more than 1
+            "No" => {
+                ans += r - l - 1;
+                // increment l
+                l += 1;
+                // increment r if it is equal to l
+                if l == r { r += 1; }
+            },
             _ => return Err(Error::JudgeInput(s)),
         }
     }
+
+    // r is now greater than n
+    assert_eq!(r, n + 1);
+    // this means that the elements between l and n can be added to ans
+    // 1 + 2 + 3 + .. + n = n * (n + 1) / 2
+    let e = n - l;
+    ans += (e * (e + 1)) / 2;
 
     println!("! {ans}");
 
